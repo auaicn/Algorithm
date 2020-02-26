f73a4208226875cab655dc06aabcd7d4ebf95ad1
auaicn <crorowslowpp@gmail.com>
2020-02-26 13:54:43 +0900

    hamming path problem solving

---
+++ baekjoon/johyungs_pick/2479_hamming_path.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <math.h>
+
+using namespace std;
+
+char save[1010][40];
+vector<int> v[1010];
+
+int main(int argc, char const *argv[])
+{
+	int N,K; scanf("%d %d",&N,&K);
+	for (int i=0;i<N;i++){
+		scanf(" %s",save[i+1]);
+	}
+
+	int A,B; scanf("%d %d",&A,&B);
+
+
+
+	return 0;
+}
